e320dcc465cd21a5f5fddd7a878afd68deaefe23
SylveK <s.tobys@gmail.com>
2020-04-06 16:31:47 +0200

    service provider not publishes migration

--- src/LaravelPasswordPoliciesServiceProvider.php
+++ src/LaravelPasswordPoliciesServiceProvider.php
@@ -3,6 +3,8 @@
 namespace SylveK\LaravelPasswordPolicies;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Filesystem\Filesystem;
+use Illuminate\Support\Collection;
 
 use SylveK\LaravelPasswordPolicies\Commands\ClearPasswordHistory;
 
@@ -11,14 +13,19 @@ use SylveK\LaravelPasswordPolicies\Commands\ClearPasswordHistory;
 class LaravelPasswordPoliciesServiceProvider extends ServiceProvider
 {
     // -- Bootstrap the application services.
-    public function boot()
+    public function boot(Filesystem $filesystem)
     {
         if ($this->app->runningInConsole()) {
-            $this->publishes([
-                __DIR__.'/../config/password-policies.php' => config_path('password-policies.php'),
-            ], 'password-policies');
+            if (function_exists('config_path')) { // function not available and 'publish' not relevant in Lumen
 
-            $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
+                $this->publishes([
+                    __DIR__.'/../config/password-policies.php' => config_path('password-policies.php'),
+                ], 'config');
+
+                $this->publishes([
+                    __DIR__.'/../database/migrations/create_password_histories_table.php.stub' => $this->getMigrationFileName($filesystem),
+                ], 'migrations');
+            }
 
             // -- Registering package commands.
             $this->commands([
@@ -29,6 +36,23 @@ class LaravelPasswordPoliciesServiceProvider extends ServiceProvider
         // collect(config('password-policies.password_history_models')) -> map(function ($model) {
         //     class_exists($model['class']) ? $model['class']::observe(UserObserver::class) : null;
         // });
+    }
+
+    /**
+     * Returns existing migration file if found, else uses the current timestamp.
+     *
+     * @param Filesystem $filesystem
+     * @return string
+     */
+    protected function getMigrationFileName(Filesystem $filesystem): string
+    {
+        $timestamp = date('Y_m_d_His');
+
+        return Collection::make($this->app->databasePath().DIRECTORY_SEPARATOR.'migrations'.DIRECTORY_SEPARATOR)
+            ->flatMap(function ($path) use ($filesystem) {
+                return $filesystem->glob($path.'*_create_password_histories_tables.php');
+            })->push($this->app->databasePath()."/migrations/{$timestamp}_create_password_histories_tables.php")
+            ->first();
     }
 
     // -- Register the application services.
